3d83a78520e0fe901a3ff27b1fe097eca39d334e
xiangtai <xiangtai@xiangtaideMac-mini.local>
2015-06-01 09:43:14 +0800

    Initial Commit

---
+++ 下拉放大/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  下拉放大
+//
+//  Created by xiangtai on 15/6/1.
+//  Copyright (c) 2015年 xiangtai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 下拉放大/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  下拉放大
+//
+//  Created by xiangtai on 15/6/1.
+//  Copyright (c) 2015年 xiangtai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
